dd8274fb197e4b7a65505e163828d1acf2cbc6a4
Gabrael Levine <gabrael@stanford.edu>
2023-08-31 20:52:14 -0700

    Push local changes

--- README.md
+++ README.md
@@ -21,3 +21,61 @@ Furthermore, common neural network libraries such as Torch are not designed to r
 - Triggers an emergency stop when safety limits are exceeded (joint velocity, body pitch, etc.)
 
 ## How to configure
+```
+# Names of the joints in the order expected by the policy
+joint_names:
+    - "left_hip"
+    - "left_knee"
+    - "left_wheel"
+    - "right_hip"
+    - "right_knee"
+    - "right_wheel"
+
+# Position gains of the joints
+kps:
+    left_hip: 20.0
+    left_knee: 20.0
+    left_wheel: 0.0
+    right_hip: 20.0
+    right_knee: 20.0
+    right_wheel: 0.0
+
+# Velocity gains of the joints
+kds:
+    left_hip: 2.0
+    left_knee: 2.0
+    left_wheel: 0.5
+    right_hip: 2.0
+    right_knee: 2.0
+    right_wheel: 0.5
+
+# Type of action for each joint (position or velocity)
+action_types:
+    left_hip: "P"
+    left_knee: "P"
+    left_wheel: "V"
+    right_hip: "P"
+    right_knee: "P"
+    right_wheel: "V"
+
+# Center of the observation/action range for each joint
+default_joint_pos:
+    left_hip: 2.0
+    left_knee: 2.0
+    left_wheel: 0.0
+    right_hip: 2.0
+    right_knee: 2.0
+    right_wheel: 0.0
+
+# Observation and action scaling factors
+obs_ang_vel_scale: 1.0
+
+obs_joint_pos_scale: 1.0
+obs_joint_vel_scale: 1.0
+
+action_joint_pos_scale: 1.0
+action_joint_vel_scale: 1.0
+
+cmd_lin_vel_scale: 1.0
+cmd_ang_vel_scale: 1.0
+```
---
+++ src/neural_controller.cpp
@@ -0,0 +1,212 @@
+#include "neural_controller/neural_controller.hpp"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "controller_interface/helpers.hpp"
+#include "hardware_interface/loaned_command_interface.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/qos.hpp"
+
+namespace neural_controller
+{
+  NeuralController::NeuralController()
+      : controller_interface::ControllerInterface(),
+        rt_command_ptr_(nullptr),
+        cmd_subscriber_(nullptr)
+  {
+  }
+
+  controller_interface::CallbackReturn NeuralController::on_init()
+  {
+    try
+    {
+      param_listener_ = std::make_shared<ParamListener>(get_node());
+      params_ = param_listener_->get_params();
+
+      std::ifstream jsonStream(params_.model_path, std::ifstream::binary);
+      model_.parseJson(jsonStream);
+      model_.reset();
+    }
+    catch (const std::exception &e)
+    {
+      fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+      return controller_interface::CallbackReturn::ERROR;
+    }
+
+    return controller_interface::CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::CallbackReturn NeuralController::on_configure(
+      const rclcpp_lifecycle::State & /*previous_state*/)
+  {
+    // Initialize the command subscriber
+    cmd_subscriber_ = get_node()->create_subscription<CmdType>(
+        "/cmd_vel", rclcpp::SystemDefaultsQoS(),
+        [this](const CmdType::SharedPtr msg)
+        { rt_command_ptr_.writeFromNonRT(msg); });
+
+    RCLCPP_INFO(get_node()->get_logger(), "configure successful");
+    return controller_interface::CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::InterfaceConfiguration
+  NeuralController::command_interface_configuration() const
+  {
+    return controller_interface::InterfaceConfiguration{
+        controller_interface::interface_configuration_type::ALL};
+  }
+
+  controller_interface::InterfaceConfiguration NeuralController::state_interface_configuration()
+      const
+  {
+    return controller_interface::InterfaceConfiguration{
+        controller_interface::interface_configuration_type::ALL};
+  }
+
+  controller_interface::CallbackReturn NeuralController::on_activate(
+      const rclcpp_lifecycle::State & /*previous_state*/)
+  {
+    rt_command_ptr_ = realtime_tools::RealtimeBuffer<std::shared_ptr<CmdType>>(nullptr);
+
+    // Populate the command interfaces map
+    for (auto &command_interface : command_interfaces_)
+    {
+      command_interfaces_map_[command_interface.get_prefix_name()].emplace(
+          command_interface.get_interface_name(),
+          std::ref(command_interface));
+    }
+
+    // Populate the state interfaces map
+    for (auto &state_interface : state_interfaces_)
+    {
+      state_interfaces_map_[state_interface.get_prefix_name()].emplace(
+          state_interface.get_interface_name(),
+          std::ref(state_interface));
+    }
+
+    RCLCPP_INFO(get_node()->get_logger(), "activate successful");
+    return controller_interface::CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::CallbackReturn NeuralController::on_deactivate(
+      const rclcpp_lifecycle::State & /*previous_state*/)
+  {
+    rt_command_ptr_ = realtime_tools::RealtimeBuffer<std::shared_ptr<CmdType>>(nullptr);
+    for (auto &command_interface : command_interfaces_)
+    {
+      command_interface.set_value(0.0);
+    }
+    RCLCPP_INFO(get_node()->get_logger(), "deactivate successful");
+    return controller_interface::CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::return_type NeuralController::update(
+      const rclcpp::Time &time, const rclcpp::Duration &period)
+  {
+    // Get the latest commanded velocities
+    auto command = rt_command_ptr_.readFromRT();
+    if (command && command->get())
+    {
+      cmd_x_vel_ = command->get()->linear.x;
+      cmd_y_vel_ = command->get()->linear.y;
+      cmd_yaw_vel_ = command->get()->angular.z;
+    }
+
+    // Get the latest observation
+    double pitch_vel, roll_vel, yaw_vel, orientation_w, orientation_x, orientation_y, orientation_z;
+    try
+    {
+      // read IMU states from hardware interface
+      pitch_vel = state_interfaces_map_.at(params_.imu_sensor_name).at("angular_velocity.y").get().get_value();
+      roll_vel = state_interfaces_map_.at(params_.imu_sensor_name).at("angular_velocity.x").get().get_value();
+      yaw_vel = state_interfaces_map_.at(params_.imu_sensor_name).at("angular_velocity.z").get().get_value();
+      orientation_w = state_interfaces_map_.at(params_.imu_sensor_name).at("orientation.w").get().get_value();
+      orientation_x = state_interfaces_map_.at(params_.imu_sensor_name).at("orientation.x").get().get_value();
+      orientation_y = state_interfaces_map_.at(params_.imu_sensor_name).at("orientation.y").get().get_value();
+      orientation_z = state_interfaces_map_.at(params_.imu_sensor_name).at("orientation.z").get().get_value();
+
+      // Calculate the projected gravity vector
+      tf2::Quaternion q(
+        orientation_x,
+        orientation_y,
+        orientation_z,
+        orientation_w);
+      tf2::Matrix3x3 m(q);
+      tf2::Vector3 world_gravity_vector(0, 0, -1);
+      tf2::Vector3 projected_gravity_vector = m.inverse() * world_gravity_vector;
+
+      // Fill the observation vector
+      // Linear velocity (zeroed out, this isn't observed)
+      observation_[0] = 0.0;
+      observation_[1] = 0.0;
+      observation_[2] = 0.0;
+      // Angular velocity
+      observation_[3] = pitch_vel * params_.obs_ang_vel_scale;
+      observation_[4] = roll_vel * params_.obs_ang_vel_scale;
+      observation_[5] = yaw_vel * params_.obs_ang_vel_scale;
+      // Projected gravity vector
+      observation_[6] = projected_gravity_vector[0];
+      observation_[7] = projected_gravity_vector[1];
+      observation_[8] = projected_gravity_vector[2];
+      // Commands
+      observation_[9] = cmd_x_vel_ * params_.cmd_lin_vel_scale;
+      observation_[10] = cmd_y_vel_ * params_.cmd_lin_vel_scale;
+      observation_[11] = cmd_yaw_vel_ * params_.cmd_ang_vel_scale;
+      // Joint positions
+      for (int i = 0; i < ACTION_SIZE; i++)
+      {
+        observation_[12 + i] = wrap_angle((state_interfaces_map_.at(params_.joint_names[i]).at("position").get().get_value() - params_.default_joint_pos[i]) * params_.obs_joint_pos_scale, -2 * M_PI, 2 * M_PI);
+      }
+      // Joint velocities
+      for (int i = 0; i < ACTION_SIZE; i++)
+      {
+        observation_[12 + ACTION_SIZE + i] = state_interfaces_map_.at(params_.joint_names[i]).at("velocity").get().get_value() * params_.obs_joint_vel_scale;
+      }
+    }
+    catch (const std::out_of_range &e)
+    {
+      RCLCPP_INFO(get_node()->get_logger(), "failed to read joint states from hardware interface");
+      return controller_interface::return_type::OK;
+    }
+
+    // Perform policy inference
+    model_.forward(observation_);
+
+    // Process the actions
+    const double* policy_output = model_.getOutputs();
+    for (int i = 0; i < ACTION_SIZE; i++)
+    {
+      // Copy policy_output to the observation vector
+      observation_[12 + ACTION_SIZE * 2 + i] = policy_output[i];
+      // Scale and de-normalize to get the action vector
+      if (params_.action_type == "position")
+      {
+        action_[i] = policy_output[i] * params_.action_scale + params_.default_joint_pos[i];
+      }
+      else {
+        action_[i] = policy_output[i] * params_.action_scale;
+      }
+      // Send the action to the hardware interface
+      command_interfaces_map_.at(params_.joint_names[i]).at(params_.action_type).get().set_value(action_[i]);
+    }
+
+    return controller_interface::return_type::OK;
+  }
+
+  double NeuralController::wrap_angle(double angle, double angle_min, double angle_max)
+  {
+      /// Wraps an angle to the range [angle_min, angle_max] ///
+      double span = angle_max - angle_min;
+      return angle - span * floor((angle - angle_min) / span);
+  }
+
+} // namespace neural_controller
+
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(
+    neural_controller::NeuralController,
+    controller_interface::ControllerInterface)
