0ce2cf12f96b2b73700365f2fabd66f5b0d89484
Nathan Kau <nathankau@gmamil.com>
2024-09-21 00:28:29 +0900

    use state publisher in sim launch

--- launch/launch_sim.py
+++ launch/launch_sim.py
@@ -25,6 +25,13 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
     robot_controllers = ParameterFile(PathJoinSubstitution(
         [
             FindPackageShare("neural_controller"),
@@ -50,7 +57,7 @@ def generate_launch_description():
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
+        parameters=[robot_controllers],
         output="both",
     )
 
@@ -66,7 +73,21 @@ def generate_launch_description():
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager", "--controller-manager-timeout", "30"],
     )
 
+    imu_sensor_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "imu_sensor_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+            "--controller-manager-timeout",
+            "30",
+        ],
+    )
+
     nodes = [
+        robot_state_publisher,
+        # imu_sensor_broadcaster_spawner,
         control_node,
         robot_controller_spawner,
         joint_state_broadcaster_spawner,
